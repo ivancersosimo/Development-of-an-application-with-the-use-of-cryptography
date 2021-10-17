65c7f59330c2b787f55bfaf53ee8a7d47663b763
xion16lm <44240573+xion16lm@users.noreply.github.com>
2021-10-17 18:40:11 +0200

    Add skeleton file

---
+++ skeleton.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def gen_symk(randomness):
+	#generate key
+	return symk
+
+def gen_asymkpair(randomness):
+	#generate keypair
+	return pub, secret
+
+
+
+def symmetric_enc(data):
+	# use a library for symmetric encryption
+
+
+	return enc_data
+
+
+def asymmetric_enc(data):
+	# use a library for symmetric encryption
+
+
+	return enc_data
+
+def encode_output(encrypted_data):
+	# encode output
+	return encoded_enc_data
+
+
+
+def main():
+
+	if len(sys.argv) < 2:
+		print('provide an input file')
+		exit(1)
+	
+
+main()
