ce1cb9179132446d586240759d9ea7a606cd0510
xion16lm <100383016@alumnos.uc3m.es>
2021-10-23 21:56:45 +0200

    Add the basic functionality of the server and askInformation method in the client

---
+++ channel.py
@@ -0,0 +1,9 @@
+class channel:
+    def __init__(self, server, client):
+        self.server = server
+        self.client = client
+    def clientToServer(self,data):
+        clientOutput = self.client.askInformation(data)
+        return serverInput
+    def serverToClient(self):
+        return clientInput
--- client.py
+++ client.py
@@ -1,4 +1,20 @@
+from skeleton_simmetric import symmetric_enc
 class client:
     def __init__(self,username,password):
         self.username = username
         self.password = password
+        self.existance = False
+
+    
+    def checkUserExistance(self):
+        return clientOutput
+
+
+    
+    def askInformation(self, data):
+        encryptedData = symmetric_enc(data)
+        hashData = hash(data)
+        clientOutput = []
+        clientOutput.append(encryptedData)
+        clientOutput.append(hashData)
+        return clientOutput
