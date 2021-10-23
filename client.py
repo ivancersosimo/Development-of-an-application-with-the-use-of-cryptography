ca365f12b27b0b570d7a1a34476caf01cf5be275
xion16lm <100383016@alumnos.uc3m.es>
2021-10-23 22:03:57 +0200

    Addition of changes in client

--- client.py
+++ client.py
@@ -6,15 +6,15 @@ class client:
         self.existance = False
 
     
-    def checkUserExistance(self):
-        return clientOutput
-
-
-    
     def askInformation(self, data):
         encryptedData = symmetric_enc(data)
         hashData = hash(data)
         clientOutput = []
         clientOutput.append(encryptedData)
         clientOutput.append(hashData)
+        return clientOutput
+    
+    def checkUserExistance(self):
+        data = ["users", self.username, self.password]
+        clientOutput = self.askInformation(data)
         return clientOutput
