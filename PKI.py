e708f393b7f30281ac6c789eb892d310deac4949
xion16lm <100383016@alumnos.uc3m.es>
2021-12-04 22:19:35 +0100

    Add channel functionalities for asymmetric and pki skeleton of retrieve_key function

--- PKI.py
+++ PKI.py
@@ -3,5 +3,6 @@ class PKI:
     def __init__(self):
         self.databasePath=pd.read_csv("publicKeys.csv")
     
-    def retrieve_key():
-        pass
+    def retrieve_key(idOwner):
+        pubkey_path=False #Until completely developed
+        return pubkey_path
