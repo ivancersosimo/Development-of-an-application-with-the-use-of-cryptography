6ef729914a5de6cd362c7219dfd46c9c548bfd4f
Ivan Dario Cersosimo <ivancersosimo26@gmail.com>
2021-12-06 18:19:50 +0100

    retrieve_key

--- PKI.py
+++ PKI.py
@@ -1,8 +1,19 @@
 import pandas as pd
 class PKI:
     def __init__(self):
-        self.databasePath=pd.read_csv("publicKeys.csv")
-    
+        self.databasePath= "keys/publicKeys.csv"
+
     def retrieve_key(idOwner):
-        pubkey_path=False #Until completely developed
+        with open(self.databasePath, 'rb') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        pubkey_path=False
+        header = next(csv_reader)
+        for row in csv_reader:
+            if (row[0] == idOwner):
+                pubkey_path = row[1]
+            elif row[0] == "Default" and pubkey_path == False:
+                pubkey_path = row[1]
+
         return pubkey_path
